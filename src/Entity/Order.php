d6a14f74582e537b696313353ce27e70a96415cf
David Terneus <davidterneus@gmail.com>
2023-01-16 18:55:33 -0500

    Create entities with wizard

---
+++ src/Entity/Order.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $timestamp = null;
+
+    #[ORM\Column]
+    private ?bool $completed = null;
+
+    #[ORM\ManyToMany(targetEntity: Product::class, inversedBy: 'orders')]
+    private Collection $basket;
+
+    public function __construct()
+    {
+        $this->basket = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTimestamp(): ?\DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeInterface $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function isCompleted(): ?bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): self
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getBasket(): Collection
+    {
+        return $this->basket;
+    }
+
+    public function addBasket(Product $basket): self
+    {
+        if (!$this->basket->contains($basket)) {
+            $this->basket->add($basket);
+        }
+
+        return $this;
+    }
+
+    public function removeBasket(Product $basket): self
+    {
+        $this->basket->removeElement($basket);
+
+        return $this;
+    }
+}
