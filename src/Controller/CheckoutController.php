b9c06eeea8dbf6395f7c1c68e0a4a78376a865b1
David Terneus <davidterneus@gmail.com>
2023-02-22 11:17:44 -0500

    Init data fixtures

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,43 +14,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class CheckoutController extends AbstractController
 {
     #[Route('/', name: 'app_checkout')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
-        $productCategory = new Category();
-        $productCategory->setName("Category");
+        $products = $productRepository->findAll();
+        dump($products);
 
-        $product1 = new Product();
-        $product1->setName('Shampoo');
-        $product1->setDescription('Test Product');
-        $product1->setPrice('3.99');
-
-        $product2 = new Product();
-        $product2->setName('Conditioner');
-        $product2->setDescription('Test product');
-        $product2->setPrice('3.99');
-
-        $productCategory->addProduct($product1);
-        $productCategory->addProduct($product2);
-
-        $productCategory2 = new Category();
-        $productCategory2->setName("Category 2");
-
-        $product3 = new Product();
-        $product3->setName('Shampoo');
-        $product3->setDescription('Test Product');
-        $product3->setPrice('3.99');
-
-        $product4 = new Product();
-        $product4->setName('Conditioner');
-        $product4->setDescription('Test product');
-        $product4->setPrice('3.99');
-
-        $productCategory2->addProduct($product3);
-        $productCategory2->addProduct($product4);
+        $categories = $categoryRepository->findAll();
+        dump($categoryRepository);
         
         return $this->render('order/product_page.html.twig',
         array(
-            'categories' => [$productCategory, $productCategory2]
+            'categories' => $categories
         ));
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $category = new Category();
+        $category->setName('Shampoo');
+        $category->setDescription('Shampoo category description');
+        $manager->persist($category);
+
+        $product = new Product();
+        $product->setName('Redken Shampoo');
+        $product->addCategory($category);
+        $product->setPrice(17.99);
+        $manager->persist($product);
 
         $manager->flush();
     }
