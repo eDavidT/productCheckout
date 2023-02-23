669882c23db529125cf8179c521ec1b05d7b776d
David Terneus <davidterneus@gmail.com>
2023-02-23 07:45:56 -0500

    Add user entity

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\ProductFixtures as DataFixturesProductFixtures;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+    public function __construct( UserPasswordHasherInterface $passwordHasher )
+    {
+        $this->hasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername('nalmasi');
+        $user->setName('Nooshin Almasi');
+        $plaintextPassword = 'test';
+        $hashedPassword = $this->hasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+
+        $adminUser = new User();
+        $adminUser->setUsername('gandrade');
+        $adminUser->setName('Giovanny Andrade');
+        $plaintextPassword = 'test';
+        $hashedPassword = $this->hasher->hashPassword(
+            $adminUser,
+            $plaintextPassword
+        );
+        $adminUser->setPassword($hashedPassword);
+        $manager->persist($adminUser);
+        
+        $manager->flush();
+    }
+}
